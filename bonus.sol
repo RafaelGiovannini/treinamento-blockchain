9960e42d8f8b8ef8410d782b31d98e4294efd965
Rafael Giovannini <rafgm90@gmail.com>
2023-06-10 09:45:22 -0300

    Update bonus.sol

--- bonus.sol
+++ bonus.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.20;
 contract BonusContract {
 
     string public vendedor;
-    uint256 private bonus;
+    uint256 public bonus;
 
     constructor(string memory nomeVendedor,uint256 valorBonus) {
         nomeVendedor = nomeVendedor;
@@ -22,4 +22,3 @@ contract BonusContract {
 	
 }
 
-//0xb33d91DBb01B2054d32Aa00F595839E9d2e814d3
